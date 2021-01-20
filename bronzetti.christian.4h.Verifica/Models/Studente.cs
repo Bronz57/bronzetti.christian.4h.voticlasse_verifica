6df05c713000e7b6718ef170228559195134aa55
bronz57 <{chbronz88}@gmail.com>
2021-01-20 10:44:54 +0100

    Primo commit

---
+++ bronzetti.christian.4h.Verifica/Models/Studente.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bronzetti.christian._4h.Verifica.Models
+{
+    class Studente
+    {
+        private string _cognome;
+        private int _numeroAssenze =0;
+        private int[] _voti = new int [100];
+
+        public double Media;
+        public int NumeroAssenze 
+        { 
+            get
+            {
+                return _numeroAssenze;
+            }
+            
+            set
+            {
+                _numeroAssenze = value;
+            }
+        }
+
+        public string Cognome
+        {
+            get
+            {
+                return _cognome;
+            }
+
+            set
+            {
+                _cognome = value;
+            }
+        }
+        
+        public Studente(string c, int nA)
+        {
+            _cognome = c;
+            _numeroAssenze = nA;
+        }
+
+        public int InserisciAssenza()
+        {
+            NumeroAssenze++;
+            return NumeroAssenze;
+        }
+
+        public void InserisciVoto(int v, int i)
+        {
+       
+            if (v > 0 && v < 31)
+                _voti[i]= v;
+        
+        }
+
+        public double CalcolaMedia ()
+        {
+           Media = 0;
+           int i = 0;
+            int j = 0;
+            for (i=0; i < _voti.Length; i++)
+            {
+                if (_voti[i] > 0)
+                    j++;
+
+                   Media += _voti[i];
+            }
+                
+
+            Media /= j;
+
+            return Media;
+        }
+        // public override string ToString() =>$"Studente: {_cognome} \t Assenze: {_numeroAssenze}";
+
+        public override string ToString() => $"Studente: {_cognome} \t Media: {Media}";
+
+    }
+}
---
+++ bronzetti.christian.4h.Verifica/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using bronzetti.christian._4h.Verifica.Models;
+using System.IO;
+
+namespace bronzetti.christian._4h.Verifica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            StreamReader sr = new StreamReader(@"ElencoStudenti.txt");
+            
+            Studente[] vettS = new Studente[25];
+            int i;
+            string strC;
+
+            for (i = 0; i < vettS.Length; i++)
+            {
+                    strC = sr.ReadLine();
+
+                    vettS[i] = new Studente(strC, 0);
+             }
+
+            sr.Close();
+
+            vettS[1].InserisciAssenza();
+            vettS[2].InserisciAssenza();
+            vettS[2].InserisciAssenza();
+
+            vettS[0].InserisciVoto(30, 0);
+            vettS[0].InserisciVoto(25, 1);
+
+            Random n = new Random();
+            for (i = 0; i < vettS.Length; i++)
+                vettS[i].InserisciVoto(n.Next(1, 30), i);
+
+
+            for (i = 0; i < vettS.Length; i++)
+                vettS[i].CalcolaMedia();
+
+            foreach(Studente s in vettS)
+                Console.WriteLine(s);
+
+        }
+    }
+}
